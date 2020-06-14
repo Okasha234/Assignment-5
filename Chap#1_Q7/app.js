aac1b2f154b284fcf3687e74ad0f2dd26bcfcd5a
Okasha <ali.okasha98@gmail.com>
2020-06-14 23:19:28 +0500

    Assignment#5_C1

---
+++ Chap#1_Q7/app.js
@@ -0,0 +1,4 @@
+var a = +prompt("Enter the number","Table you want to print");
+for(var i=1; i<=10; i++){
+    document.write(a + "x" + i + "=" + (a*i) + "<br>");
+}
---
+++ Chapt#1_Q(1-5)/app.js
@@ -0,0 +1,14 @@
+//-------------Chapter#1 Q_1--------------//
+alert("Thank you! for visiting our page");
+
+//-------------Chapter#1 Q_2--------------//
+alert("Error! Please enter a valid password");
+
+//-------------Chapter#1 Q_3--------------//
+alert("Welcome to JS Land\nHappy Coding!");
+
+//-------------Chapter#1 Q_4--------------//
+alert("Welcome to JS Land");
+alert("Happy Coding!");
+
+console.log(alert("Hello...I can run JS through my web browser's console"))
